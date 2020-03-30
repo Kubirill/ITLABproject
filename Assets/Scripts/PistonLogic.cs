bb60b454c5e57cc4f25a3a070767bd81d2598e93
Kubirill <Kubirill@rambler.ru>
2020-03-30 21:22:49 +0300

    finish pistonLogic

--- Assets/Scripts/PistonLogic.cs
+++ Assets/Scripts/PistonLogic.cs
@@ -4,22 +4,20 @@ using UnityEngine;
 
 public class PistonLogic : MonoBehaviour
 {
-    public bool work = false;
     public GameObject level;
    
 
     private void OnMouseDown()
     {
-        level.GetComponent<pistonLevel>().OperationPiston(gameObject.GetComponent<MovingObjectPistonLevel>().Xcoor, gameObject.GetComponent<MovingObjectPistonLevel>().Ycoor, work);
+        level.GetComponent<pistonLevel>().OperationPiston(gameObject.GetComponent<MovingObjectPistonLevel>().Xcoor, gameObject.GetComponent<MovingObjectPistonLevel>().Ycoor);
     }
     public void Close()
     {
-        work = false;
         gameObject.GetComponent<Animator>().Play("close");
     }
     public void Open()
     {
-        work = true;
+        //work = true;
         gameObject.GetComponent<Animator>().Play("open");
 
     }
