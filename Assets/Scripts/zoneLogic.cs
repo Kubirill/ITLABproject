c6c80d433ccd66f5b898d30abd9e8f8a693a48af
Kubirill <Kubirill@rambler.ru>
2020-04-05 14:35:40 +0300

    finish tracery logic

--- Assets/Scripts/zoneLogic.cs
+++ Assets/Scripts/zoneLogic.cs
@@ -6,15 +6,18 @@ public struct elementPosition
     public GameObject gObj;
     public bool flipped;
     public float angle;
+    public int id;
     public static bool operator ==(elementPosition elsOne, elementPosition elsAns)
     {
-        if ((elsOne.gObj.name== elsAns.gObj.name)&& (elsOne.flipped == elsAns.flipped))
+        //Debug.Log(elsOne.angle + " " + elsAns.angle + " " + elsOne.flipped + " " + elsAns.flipped + " " + elsOne.id + " " + elsAns.id);
+        if ((elsOne.id== elsAns.id)&& (elsOne.flipped == elsAns.flipped))
             {
             int partAngle;
             if (elsOne.gObj.name == "figure1") partAngle = 60;
             else partAngle = 90;
             float angleOne = (elsOne.angle + 360) % partAngle;
             float angleAns = (elsAns.angle + 360) % partAngle;
+            
             if (Mathf.Abs(angleOne - angleAns) < 10) return true;
             else return false;
             }
@@ -28,16 +31,62 @@ public struct elementPosition
 public class zoneLogic : MonoBehaviour
 {
     public GameObject[] elements;
+    public GameObject[] answersElements;
     public GameObject cam;
+    public Transform answerBlock;
     //public GameObject[] elementsOnScene = new GameObject[3];
     public float speedRotate=1;
     elementPosition[] elementsOnScene = new elementPosition[3];
     elementPosition[] answer = new elementPosition[3];
     private int index;
+    public int level=1;
 
     private void Start()
     {
-        
+        setAnswerTraceryPazzle();
+        CreateVisualAnswer();
+    }
+
+    private void setAnswerTraceryPazzle()
+    {
+        answer[0].gObj = answersElements[0];
+        answer[0].angle = Random.Range(0, level + 1) * 60 / (level + 1);
+        answer[1].gObj = answersElements[1];
+        answer[1].angle = Random.Range(0, level + 1) * 90 / (level + 1);
+        answer[2].gObj = answersElements[2];
+        answer[2].angle = Random.Range(0, level + 1) * 90 / (level + 1);
+        bool fliping;
+        if (Random.Range(0, 2) >= 1) fliping = true;
+        else fliping = false;
+        for (int i = 0; i < 3; i++)
+        {
+            answer[i].flipped = fliping;
+            answer[i].id = i;
+        }
+        for (int first = 0; first < 3; first++)
+        {
+
+            for (int second = 0; second < 3; second++)
+            {
+                if (Random.Range(0, 2) >= 1)
+                {
+                    elementPosition trush = answer[first];
+                    answer[first] = answer[second];
+                    answer[second] = trush;
+                }
+            }
+        }
+    }
+
+    private void CreateVisualAnswer()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            int angle;
+            if (answer[i].flipped) angle = 1;
+            else angle = 0;
+            Instantiate(answer[i].gObj, answerBlock.position + new Vector3(0, 0, -0.06f - 0.01f * i+0.01f*angle), Quaternion.Euler(0, angle*180, answer[i].angle), answerBlock);
+        }
     }
 
     private void OnMouseDown()
@@ -53,6 +102,7 @@ public class zoneLogic : MonoBehaviour
                     elementsOnScene[index].gObj.GetComponent<traceryLogic>().cam = cam;
                     elementsOnScene[index].gObj.GetComponent<BoxCollider>().enabled = false;
                     elementsOnScene[index].flipped = false;
+                    elementsOnScene[index].id = elementsOnScene[index].gObj.GetComponent<traceryLogic>().id;
                     elementsOnScene[index].angle = elementsOnScene[index].gObj.transform.rotation.z;
                     index++;
                 }
@@ -89,19 +139,28 @@ public class zoneLogic : MonoBehaviour
             {
                 if (!elementsOnScene[index - 1].flipped) elementsOnScene[index - 1].gObj.transform.Rotate(new Vector3(0, 0, 1), speedRotate);
                 else elementsOnScene[index - 1].gObj.transform.Rotate(new Vector3(0, 0, 1), -speedRotate);
-                elementsOnScene[index-1].angle = elementsOnScene[index].gObj.transform.rotation.z;
+                elementsOnScene[index-1].angle = elementsOnScene[index-1].gObj.transform.localEulerAngles.z;
+
             }
             if (Input.GetKey(KeyCode.E))
             {
 
                 if (!elementsOnScene[index - 1].flipped)  elementsOnScene[index - 1].gObj.transform.Rotate(new Vector3(0, 0, 1), -speedRotate);
                 else elementsOnScene[index - 1].gObj.transform.Rotate(new Vector3(0, 0, 1), speedRotate);
-                elementsOnScene[index-1].angle = elementsOnScene[index-1].gObj.transform.rotation.z;
+                elementsOnScene[index-1].angle = elementsOnScene[index-1].gObj.transform.localEulerAngles.z;
             }
         }  
         if (index == 3)
         {
-
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                
+                for (int i=0; i < 3; i++)
+                {
+                    if(elementsOnScene[i]==answer[i]) Debug.Log("equal");
+                    else Debug.Log("not");
+                }
+            }
         }
     }
 }
